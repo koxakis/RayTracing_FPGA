f1698536112eb0101657fa51f19d50bf9f936322
Nikolas Koxenoglou <koxakin@gmail.com>
2021-01-30 23:50:26 +0200

    Inlined ray triangle intersection function

--- software_implementation/software_optimized_shading_system/shading.cpp
+++ software_implementation/software_optimized_shading_system/shading.cpp
@@ -605,8 +605,18 @@ public:
 					inputTriangleVertexPos[i].y = P[i].y ;
 					inputTriangleVertexPos[i].z = P[i].z ;
 				}	
+			float local_t;
+			float local_u;
+			float local_v;
 
-			j = 0;
+			float orig_x  = inputStaticArray[0];
+			float orig_y  = inputStaticArray[1];
+			float orig_z  = inputStaticArray[2];
+
+			float dir_x  = inputStaticArray[3];
+			float dir_y  = inputStaticArray[4];
+			float dir_z  = inputStaticArray[5];
+
 			// NEW PERIPHERAL SATART
 			for (uint32_t i = 0; i < numTris; ++i) 
 				{
@@ -634,12 +644,115 @@ public:
 					/* a ray may intersect more than one triangle from the mesh therefore we also 
 					need to keep track of the nearest intersection distance as we iterate over the triangles.            
 					*/
+					/*
 					temp_ret = rayTriangleIntersect(inputStaticArray[0], inputStaticArray[1], inputStaticArray[2], 
 																					inputStaticArray[3], inputStaticArray[4], inputStaticArray[5], 
 																					v0_x, v0_y, v0_z, 
 																					v1_x, v1_y, v1_z, 
 																					v2_x, v2_y, v2_z, 
-																					t, u, v);
+																					t, u, v); 
+					*/
+					// find if the ray intersects the triangle 
+
+					// find the triangles normal
+					// E1 in equation
+					//Vec3f v0v1 = v1 - v0;
+					float v0v1_x = v1_x - v0_x;
+					float v0v1_y = v1_y - v0_y;
+					float v0v1_z = v1_z - v0_z;
+					// E2 in equation
+					//Vec3f v0v2 = v2 - v0;
+					float v0v2_x = v2_x - v0_x;
+					float v0v2_y = v2_y - v0_y;
+					float v0v2_z = v2_z - v0_z;
+					// P in the equation
+					// y * v.z - z * v.y, 
+					// z * v.x - x * v.z, 
+					// x * v.y - y * v.x
+					//Vec3f pvec = dir.crossProduct(v0v2);
+					float pvec_x = (dir_y * v0v2_z) - (dir_z * v0v2_y);
+					float pvec_y = (dir_z * v0v2_x) - (dir_x * v0v2_z);
+					float pvec_z = (dir_x * v0v2_y) - (dir_y * v0v2_x);
+					// the user might want to cull (discard) back-facing triangles
+					// if the triangle is front-facing the determinant is positive otherwise it is negative
+					// P*E1 in the equation 
+					// x * v.x + y * v.y + z * v.z
+					//float det = v0v1.dotProduct(pvec);
+					float det = (v0v1_x * pvec_x) + (v0v1_y * pvec_y) + (v0v1_z * pvec_z);
+
+					// Deactivate CULLING in order to render back facing triangles as well
+					// Useful for secondary rays can be neglected for primary rays 
+#ifdef CULLING
+						// if the determinant is negative the triangle is backfacing
+						// if the determinant is close to 0, the ray misses the triangle
+						if (det < kEpsilon) return false;
+#else
+						// ray and triangle are parallel if det is close to 0
+						if ((det > -kEpsilon) && (det < kEpsilon))
+							{
+								temp_ret = false;
+								j += 3;
+								continue;
+							}
+#endif
+
+					// compute once and multiply to find u,v and t
+					// 1/P*E1 in the equation 
+					float invDet = 1 / det;
+
+					// translate to the unit triangle 
+					float tvec_x = orig_x - v0_x;
+					float tvec_y = orig_y - v0_y;
+					float tvec_z = orig_z - v0_z;
+					// compute u from (T dotproduct P) * 1/P*E1
+					local_u = ( (tvec_x * pvec_x) + (tvec_y * pvec_y) + (tvec_z * pvec_z)) * invDet;
+					//u = tvec.dotProduct(pvec) * invDet;
+					// we reject the triangle if u is either lower than 0 or greater than 1
+					if (local_u< 0 || local_u > 1) 
+						{
+							u = local_u;
+							temp_ret = false;
+							j += 3;
+							continue;
+						}
+
+					// Q in the equation 
+					//Vec3f qvec = tvec.crossProduct(v0v1);
+					float qvec_x = (tvec_y * v0v1_z) - (tvec_z * v0v1_y);
+					float qvec_y = (tvec_z * v0v1_x) - (tvec_x * v0v1_z);
+					float qvec_z = (tvec_x * v0v1_y) - (tvec_y * v0v1_x);
+					// compute v from (D dotproduct Q) * 1/P*E1 
+					//v = dir.dotProduct(qvec) * invDet;
+					local_v = ( (dir_x * qvec_x) + (dir_y * qvec_y) + (dir_z * qvec_z)) * invDet;
+					// we reject the triangle if v is either lower than 0 or greater than 1
+					if (local_v < 0 || local_u + local_v > 1) 
+						{
+							v = local_v;
+							u = local_u;
+							temp_ret = false;
+							j += 3;
+							continue;
+						}
+
+					// compute t from (E2 dotproduct Q) * 1/P*E1 
+					//t = v0v2.dotProduct(qvec) * invDet;
+					local_t = ( (v0v2_x * qvec_x) + (v0v2_y * qvec_y) + (v0v2_z * qvec_z)) * invDet;
+					
+					if ( local_t > 0)
+						{
+							v = local_v;
+							u = local_u;
+							t = local_t;
+							temp_ret = true;
+						}
+					else
+						{
+							v = local_v;
+							u = local_u;
+							t = local_t;	
+							temp_ret = false;
+						}
+					
 					if ( (temp_ret) && t < tNear) 
 						{
 							tNear = t;
